2dde7b31da68af8d324f4680d0aa03e120118392
Bernardo Stein <stein.bernardo@proton.me>
2023-08-24 17:21:55 -0300

    ajuste na logica da funcao renderRepos

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,51 +1,57 @@
-export const renderUserCard = (userInfo) => {
+export const renderUserCard = () => {
+  const userData = JSON.parse(
+    localStorage.getItem("@gitSearch:githubUserInfo")
+  );
 
-  const userData = JSON.parse(localStorage.getItem("@gitSearch:githubUserInfo"))
+  const header = document.querySelector(".profile__header");
 
-  const header = document.querySelector('.profile__header')
+  const profileCard = document.createElement("div");
+  profileCard.classList.add("profile__info");
 
-  const profileCard = document.createElement('div')
-  profileCard.classList.add('profile__info')
+  const userImg = document.createElement("img");
+  userImg.classList.add("profile__image");
+  userImg.src = userData.avatar;
 
-  const userImg = document.createElement('img')
-  userImg.classList.add('profile__image')
-  userImg.src = userData.avatar
+  const userName = document.createElement("p");
+  userName.classList.add("profile__username");
+  userName.innerText = userData.name;
 
+  const changeUser = document.createElement("button");
+  changeUser.classList.add("profile__change-user--button");
+  changeUser.innerText = "Trocar de usuário";
 
-  const userName = document.createElement('p')
-  userName.classList.add('profile__username')
-  userName.innerText = userData.name
+  profileCard.append(userImg, userName);
+  header.append(profileCard, changeUser);
+};
 
-  const changeUser = document.createElement('button')
-  changeUser.classList.add('profile__change-user--button')
-  changeUser.innerText = 'Trocar de usuário'
+export const renderRepos = () => {
+  const userData = JSON.parse(
+    localStorage.getItem("@gitSearch:githubUserInfo")
+  );
+  const repoList = document.querySelector(".profile__ul");
 
-  profileCard.append(userImg, userName)
-  header.append(profileCard, changeUser)
-}
+  userData.repos.forEach((repo) => {
+    console.log(repo);
+    const listItem = document.createElement("li");
 
+    const repoTitle = document.createElement("h4");
+    repoTitle.innerText = repo.title;
 
-export const renderRepos = (userInfo) => {
-  const userData = JSON.parse(localStorage.getItem("@gitSearch:githubUserInfo"))
-  const repoList = document.querySelector('.profile__ul')
-  
-  
-  userData.repos.forEach( repo => {
-    console.log(repo)
-    const listItem = document.createElement('li')
-
-    const repoTitle = document.createElement('h4')
-    repoTitle.innerText = repo.title
-
-    const repoDescription = document.createElement('p')
-    repoDescription.innerText = repo.description
-
-    const repoLink = document.createElement('a')
-    repoLink.innerText = "Abrir repositório"
-    repoLink.href = repo.url
-    repoLink.target = "_blank"
+    const repoDescription = document.createElement("p");
     
-    listItem.append(repoTitle, repoDescription, repoLink)
-    repoList.appendChild(listItem)
+    if(repo.description === null) {
+      repoDescription.innerText = "Este repositório não possui uma descrição..." 
+      repoDescription.classList.add('no-description')
+    } else {
+      repoDescription.innerText = repo.description;
+    }
+
+    const repoLink = document.createElement("a");
+    repoLink.innerText = "Abrir repositório";
+    repoLink.href = repo.url;
+    repoLink.target = "_blank";
+
+    listItem.append(repoTitle, repoDescription, repoLink);
+    repoList.appendChild(listItem);
   });
-}
+};
